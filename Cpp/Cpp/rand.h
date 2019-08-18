f8b8395fa300c958b6879dbb9e157fff8dc50c31
KB <kbm0996@naver.com>
2019-08-18 16:00:19 +0900

    add comment

--- Cpp/Cpp/rand.h
+++ Cpp/Cpp/rand.h
@@ -2,6 +2,11 @@
 #define __MYRAND_H__
 #include <random>
 
+/************************************************ 
+ C의 rand() 함수는 난수 퀄리티가 많이 떨어짐. 
+따라서, C++에서는 random_device 사용 권장 
+*************************************************/
+
 inline int64_t rand(const int64_t min, const int64_t max)
 {
 	static std::random_device	rn;			// 외부 장치에서 랜덤 시퀀스 생성
